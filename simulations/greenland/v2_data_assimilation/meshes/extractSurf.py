5481610a0e767e3467ff511917e113c8c86e05dc
pf4d <evan.cummings@umontana.edu>
2013-11-27 08:49:15 -0700

    extracting surf

--- simulations/greenland/v2_data_assimilation/meshes/extractSurf.py
+++ simulations/greenland/v2_data_assimilation/meshes/extractSurf.py
@@ -1,32 +1,31 @@
 from dolfin import *
 
-"""
-mesh = UnitSquareMesh(10,10)
-V = FunctionSpace(mesh, "CG", 1)
-u = Function(V)
-u.vector()[:] = 1.5
-
-bmesh = BoundaryMesh(mesh, "exterior")
-mapping = bmesh.entity_map(1) 
-part_of_boundary = CellFunction("size_t", bmesh, 0)
-for cell in cells(bmesh):
-  if Facet(mesh, mapping[cell.index()]).normal().x() < 0:
-      part_of_boundary[cell] = 1
-
-submesh_of_boundary = SubMesh(bmesh, part_of_boundary, 1)
-Vb = FunctionSpace(submesh_of_boundary, "CG", 1)
-ub = Function(V)
-ub.interpolate(u)
-File("ub.pvd") << ub
-"""
+def extract_surf():
+  mesh = UnitCubeMesh(10,10,10)
+  V = FunctionSpace(mesh, "CG", 1)
+  u = Function(V)
+  u.vector()[:] = 1.5
+  
+  bmesh = BoundaryMesh(mesh, "exterior")
+  mapping = bmesh.entity_map(1) 
+  part_of_boundary = CellFunction("size_t", bmesh, 0)
+  for cell in cells(bmesh):
+    if Facet(mesh, mapping[cell.index()]).normal().x() < 0:
+        part_of_boundary[cell] = 1
+  
+  submesh_of_boundary = SubMesh(bmesh, part_of_boundary, 1)
+  Vb = FunctionSpace(submesh_of_boundary, "CG", 1)
+  ub = Function(V)
+  ub.interpolate(u)
+  File("ub.pvd") << ub
 
 mesh = Mesh('mesh.xml')
 V    = FunctionSpace(mesh, "CG", 1)
 u    = Function(V)
-File('../results_sq/00/beta2_opt.xml') >> u
+File('../results_sq/03/beta2_opt.xml') >> u
 
 bmesh   = BoundaryMesh(mesh, "exterior")
-mapping = bmesh.entity_map(1)
+mapping = bmesh.entity_map(2)
 part_of_boundary = CellFunction("size_t", bmesh, 0)
 
 for cell in cells(bmesh):

34744d9c6eaf4327f4ff7a9a8570ccbf5b3329bd
Evan Cummings <evan.cummings@umontana.edu>
2015-11-09 15:35:05 -0700

    updated assimilation

--- simulations/greenland/data_assimilation/jakobshavn/data_assimilation.py
+++ simulations/greenland/data_assimilation/jakobshavn/data_assimilation.py
@@ -12,24 +12,32 @@ dir_b   = 'dump/jakob_da_ipopt_SIA0_SR/0'
 
 # set the output directory :
 in_dir  = dir_b + str(i) + '/thermo_solve/xml/'  # previous thermo_solve.py run
-out_dir = dir_b + str(i) + '/inverted_logL2_UNI0/' # new output directory
+out_dir = dir_b + str(i) + '/inverted_logL2_TV/' # new output directory
 var_dir = 'dump/vars_jakobshavn/'                # gen_vars.py ouput state.h5
 
 # get the data created with gen_vars.py :
-f = HDF5File(mpi_comm_world(), var_dir + 'state.h5', 'r')
+fdata   = HDF5File(mpi_comm_world(), var_dir + 'state.h5', 'r')
+fmeshes = HDF5File(mpi_comm_world(), var_dir + 'submeshes.h5', 'r')
+
+# get the bed mesh :
+bedmesh = Mesh()
+fmeshes.read(bedmesh, 'bedmesh', False)
+
+# create bed function space for saving beta opt :
+Qb = FunctionSpace(bedmesh, 'CG', 1)
 
 # initialize the model :
-model = D3Model(f, out_dir + 'pvd/')
-model.set_subdomains(f)
+model = D3Model(fdata, out_dir + 'pvd/')
+model.set_subdomains(fdata)
 
 # initialize variables :
-model.init_S(f)
-model.init_B(f)
-model.init_mask(f)
+model.init_S(fdata)
+model.init_B(fdata)
+model.init_mask(fdata)
 model.init_q_geo(model.ghf)
-model.init_T_surface(f)
-model.init_adot(f)
-model.init_U_ob(f, f)
+model.init_T_surface(fdata)
+model.init_adot(fdata)
+model.init_U_ob(fdata, fdata)
 model.init_E(1.0)
 model.init_u_lat(0.0)
 model.init_v_lat(0.0)
@@ -37,8 +45,8 @@ model.init_v_lat(0.0)
 # use T0 and beta0 from the previous thermo_solve :
 model.init_T(in_dir + 'T.xml')          # temp
 model.init_W(in_dir + 'W.xml')          # water
-#model.init_beta(in_dir + 'beta.xml')    # friction
-model.init_beta(3000)
+model.init_beta(in_dir + 'beta.xml')    # friction
+#model.init_beta(3000)
 model.init_U(in_dir + 'u.xml',
              in_dir + 'v.xml',
              in_dir + 'w.xml')
@@ -74,15 +82,18 @@ J = mom.form_obj_ftn(integral=model.dSrf_g, kind='log_L2_hybrid',
 #J = mom.form_obj_ftn(integral=model.dSrf_g, kind='ratio') 
 
 # form the regularization functional :
-R = mom.form_reg_ftn(model.beta, integral=model.dBed_g, kind='Tikhonov', 
-                     alpha=100.0)
+R = mom.form_reg_ftn(model.beta, integral=model.dBed_g, kind='TV', 
+                     alpha=1.0)
 
 # define the objective functional to minimize :
 I = J + R
 
 # create a pvd file for saving current optimized traction :
-controls = File(model.out_dir + "control_viz/beta_control.pvd")
-beta_viz = Function(model.Q, name="beta_control")
+beta_b = File(model.out_dir + "control_viz/beta_control.pvd")
+beta_viz = Function(Qb, name="beta_control")
+
+# create counter to save .xml and .pvd files ;
+i = 0
 
 # objective function callback function : 
 def eval_cb(I, beta):
@@ -93,11 +104,17 @@ def eval_cb(I, beta):
 
 # derivative of objective function callback function : 
 def deriv_cb(I, dI, beta):
+  global i
   #print_min_max(I,     'I')
   print_min_max(dI,    'dI/dbeta')
   #print_min_max(beta,  'beta')
-  beta_viz.assign(beta)
-  model.save_pvd(beta_viz, 'beta_control', f_file=controls) 
+  if i % 100 == 0:
+    model.assign_submesh_variable(beta_viz, beta)
+    model.set_out_dir(out_dir = out_dir + 'xml/')
+    model.save_xml(beta_viz, 'beta_control_%s' % str(i))
+    model.set_out_dir(out_dir = out_dir + 'pvd/')
+    model.save_pvd(beta_viz, 'beta_control', f_file=beta_b) 
+  i += 1
 
 # hessian of objective function callback function (not used) :
 def hessian_cb(I, ddI, beta):

3c063f5af8c009ebdaa1093c340939b39d544038
Evan Cummings <cummings.evan@gmail.com>
2016-08-09 10:06:13 -0500

    added setup.py in case you want to install it

--- setup.py
+++ setup.py
@@ -1,14 +1,11 @@
 from distutils.core import setup
 
-setup
-(      
-  name        ='cslvr',
-  version     ='2016.1.0',
-  description ='Cryospheric Problem Solver',
-  author      ='Evan Cummings, Douglas Brinkerhoff, Jesse Johnson',
-  url         ='https://github.com/pf4d/clsvr',
-  packages    =['cslvr', 
-                'tifffile'],
-  package_dir ={'cslvr'                 : 'cslvr',
-                'tifffile'              : 'cslvr/ext_scripts'}
-)
+setup(  name        = 'cslvr',
+        version     = '2016.1.0',
+        description = 'Cryospheric Problem Solver',
+        author      = 'Evan Cummings, Douglas Brinkerhoff, Jesse Johnson',
+        url         = 'https://github.com/pf4d/clsvr',
+        packages    = ['cslvr', 
+                       'tifffile'],
+        package_dir = {'cslvr'                 : 'cslvr',
+                      'tifffile'              : 'cslvr/ext_scripts'}  )
